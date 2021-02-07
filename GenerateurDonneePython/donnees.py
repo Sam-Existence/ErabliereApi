6f927c408fbe5b24319281495180129a9d1e74f3
Frederic Jacques <gary.bob88@gmail.com>
2021-02-07 01:30:54 +0000

    création github action

--- GenerateurDonneePython/donnees.py
+++ GenerateurDonneePython/donnees.py
@@ -10,7 +10,7 @@ def temperature(t):
   hod = t.timetuple()[3]
   return int((12 * math.sin((doy-105)*math.pi/182.5)) + (5*math.sin(doy*4)) + (5 * math.sin((hod - 7) * 3.14159 / 12)))
 
-url = "http://192.168.0.103:5000/erablieres/0/Donnees"
+url = "https://freddycoder.com/erablieres/0/Donnees"
 t = temperature(datetime.datetime.now())
 donnees = {'t': t, 'nb': 0, 'v': 0, 'idErabliere': 0}
 h = {"Content-Type":"Application/json"}
