0fe67ba6cecb09d324482bafc53b7d8f8b51d432
Frédéric Jacques <gary.bob88@gmail.com>
2021-04-03 15:55:12 -0400

    ajout client id prod

--- ErabliereIU/src/environments/environment.prod.ts
+++ ErabliereIU/src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   apiUrl: "https://erabliereapi.freddycoder.com",
   stsAuthority: "https://oauth2.freddycoder.com",
+  clientId: "erabliereiu",
   appRoot: "https://erablireapi.freddycoder.com",
   enableAuth: true
 };
