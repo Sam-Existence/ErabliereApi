d3bfad2a438cf95d1059952937bf28b83f59af5a
Frédéric Jacques <gary.bob88@gmail.com>
2021-12-29 13:15:23 -0500

    ajouter un indicateur de chargement

--- ErabliereIU/src/erablieres/erabliere.component.ts
+++ ErabliereIU/src/erablieres/erabliere.component.ts
@@ -9,7 +9,8 @@ import { Erabliere } from 'src/model/erabliere';
     templateUrl: 'erabliere.component.html'
 })
 export class ErabliereComponent implements OnInit {
-    erablieres: Array<Erabliere> = [];
+    erablieres?: Array<Erabliere>;
+    etat: string = "Initialisation...";
 
     erabliereSelectionnee?:Erabliere;
 
@@ -35,31 +36,37 @@ export class ErabliereComponent implements OnInit {
             }
         });
 
-        const erablieres = await this._erabliereApi.getErablieresExpandCapteurs();
+        const erablieres = await (this._erabliereApi.getErablieresExpandCapteurs().catch(err => {
+            console.log(err);
+            this.etat = "Erreur lors du chargement des érablieres";
+        }));
 
-        this.erablieres = erablieres.sort((a, b) => {
-            if (a.indiceOrdre != null && b.indiceOrdre == null)
-            {
-                return -1;
-            }
-            else if (b.indiceOrdre != null && a.indiceOrdre == null)
-            {
-                return 1;
-            }
-            else if (a.indiceOrdre != null && b.indiceOrdre != null)
-            {
-                return a.indiceOrdre - b.indiceOrdre;
-            }
+        if (erablieres != null) {
+            this.erablieres = erablieres.sort((a, b) => {
+                if (a.indiceOrdre != null && b.indiceOrdre == null)
+                {
+                    return -1;
+                }
+                else if (b.indiceOrdre != null && a.indiceOrdre == null)
+                {
+                    return 1;
+                }
+                else if (a.indiceOrdre != null && b.indiceOrdre != null)
+                {
+                    return a.indiceOrdre - b.indiceOrdre;
+                }
 
-            return a.nom?.localeCompare(b.nom ?? "") ?? 0;
-        });
+                return a.nom?.localeCompare(b.nom ?? "") ?? 0;
+            });
 
-        if (this.erablieres.length > 0) {
-            this.erabliereSelectionnee = this.erablieres[0];
-            this.idSelectionnee = this.erabliereSelectionnee.id;
-        }
-        else {
-            // TODO : Aucun érablière trouvé
+            if (this.erablieres.length > 0) {
+                this.etat = "Chargement des erablieres terminé";
+                this.erabliereSelectionnee = this.erablieres[0];
+                this.idSelectionnee = this.erabliereSelectionnee.id;
+            }
+            else {
+                this.etat = "Aucune erablière";
+            }
         }
     }
 
