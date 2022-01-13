e28544ab8efe54a3072c394dc94ce636e640c3b0
Frédéric Jacques <gary.bob88@gmail.com>
2022-01-12 21:08:52 -0500

    add user secret, remove used using

--- ErabliereApi/Program.cs
+++ ErabliereApi/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Text.Json;
 
 namespace ErabliereApi;
 
