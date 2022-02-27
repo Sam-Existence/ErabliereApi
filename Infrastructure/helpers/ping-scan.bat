95e596e6bdb4fb40d6786b8cc01654da8250c399
Frédéric Jacques <gary.bob88@gmail.com>
2022-02-27 18:48:38 -0500

    edit ping scan

--- Infrastructure/helpers/ping-scan.bat
+++ Infrastructure/helpers/ping-scan.bat
@@ -1,7 +1,11 @@
-# Scan a network
+:: Scan a network
+
+for /L %%x in (1,1,254) do ping -w 30 -n 1 192.168.0.%%x | find "TTL" >> ping-scan.txt
 
 arp -a
 
-for /L %a in (1,1,254) do start ping 192.168.0.%a
+type ping-scan.txt
 
-# Physical address may be 00-e0-62-21-7a-58
+del ping-scan.txt
+
+:: Physical address may be 00-e0-62-21-7a-58
