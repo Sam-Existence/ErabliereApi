55723a12ff0406b9816ec557869261fd3a15a045
Frédéric Jacques <gary.bob88@gmail.com>
2022-03-20 09:57:56 -0400

    fetch account info before aquireTokenSilent

--- ErabliereIU/src/authorisation/azuread-autorisation-service.ts
+++ ErabliereIU/src/authorisation/azuread-autorisation-service.ts
@@ -8,161 +8,164 @@ import { AuthResponse } from 'src/model/authresponse';
 import { IAuthorisationSerivce } from './iauthorisation-service';
 
 export class AzureADAuthorisationService implements IAuthorisationSerivce {
-    private _msalInstance: PublicClientApplication;
-    private _activeHomeAccountId?: string
-    private _loginChangedSubject = new Subject<Boolean>();
+  private _msalInstance: PublicClientApplication;
+  private _activeHomeAccountId?: string
+  private _loginChangedSubject = new Subject<Boolean>();
 
-    loginChanged = this._loginChangedSubject.asObservable();
+  loginChanged = this._loginChangedSubject.asObservable();
 
-    constructor(private _environmentService: EnvironmentService) {
-        if (this._environmentService.clientId == undefined) {
-            throw new Error("/assets/config/oauth-oidc.json/clientId cannot be null when using AzureAD authentication mode");
+  constructor(private _environmentService: EnvironmentService) {
+    if (this._environmentService.clientId == undefined) {
+      throw new Error("/assets/config/oauth-oidc.json/clientId cannot be null when using AzureAD authentication mode");
+    }
+
+    const msalConfig: Configuration = {
+      auth: {
+        clientId: this._environmentService.clientId,
+        authority: "https://login.microsoftonline.com/" + this._environmentService.tenantId,
+        redirectUri: "/signin-callback",
+        postLogoutRedirectUri: "/signout-callback",
+        navigateToLoginRequestUrl: true
+      },
+      cache: {
+        cacheLocation: BrowserCacheLocation.LocalStorage,
+        storeAuthStateInCookie: false,
+      },
+      system: {
+        loggerOptions: {
+          loggerCallback: (level: LogLevel, message: string, containsPii: boolean): void => {
+            if (containsPii) {
+              return;
+            }
+            switch (level) {
+              case LogLevel.Error:
+                console.error(message);
+                return;
+              case LogLevel.Info:
+                console.info(message);
+                return;
+              case LogLevel.Verbose:
+                console.debug(message);
+                return;
+              case LogLevel.Warning:
+                console.warn(message);
+                return;
+            }
+          },
+          piiLoggingEnabled: false
+        },
+        windowHashTimeout: 60000,
+        iframeHashTimeout: 6000,
+        loadFrameTimeout: 0,
+        asyncPopups: false
+      }
+    }
+
+    this._msalInstance = new PublicClientApplication(msalConfig);
+  }
+
+  login() {
+    const popupParam: PopupRequest = {
+      scopes: this._environmentService.scopes?.split(' ') ?? [],
+      prompt: "select_account"
+    }
+    return this._msalInstance.loginPopup(popupParam).then(async response => {
+      this._msalInstance.setActiveAccount(response.account);
+
+      this._activeHomeAccountId = response.account?.homeAccountId;
+
+      await this.completeLogin();
+    });
+  }
+
+  isLoggedIn(): Promise<Boolean> {
+    return new Promise(async (resolve, reject) => {
+      let user = this._msalInstance.getActiveAccount();
+
+      if (user == null) {
+        const users = this._msalInstance.getAllAccounts();
+
+        if (users.length > 0) {
+          user = users[0];
+          this._activeHomeAccountId = user.homeAccountId
+          this._msalInstance.setActiveAccount(user);
         }
-        
-        const msalConfig: Configuration = {
-            auth: {
-                clientId: this._environmentService.clientId,
-                authority: "https://login.microsoftonline.com/" + this._environmentService.tenantId,
-                redirectUri: "/signin-callback",
-                postLogoutRedirectUri: "/signout-callback",
-                navigateToLoginRequestUrl: true
-            },
-            cache: {
-                cacheLocation: BrowserCacheLocation.LocalStorage,
-                storeAuthStateInCookie: false,
-            },
-            system: {
-                loggerOptions: {
-                    loggerCallback: (level: LogLevel, message: string, containsPii: boolean): void => {
-                        if (containsPii) {
-                            return;
-                        }
-                        switch (level) {
-                            case LogLevel.Error:
-                                console.error(message);
-                                return;
-                            case LogLevel.Info:
-                                console.info(message);
-                                return;
-                            case LogLevel.Verbose:
-                                console.debug(message);
-                                return;
-                            case LogLevel.Warning:
-                                console.warn(message);
-                                return;
-                        }
-                    },
-                    piiLoggingEnabled: false
-                },
-                windowHashTimeout: 60000,
-                iframeHashTimeout: 6000,
-                loadFrameTimeout: 0,
-                asyncPopups: false
-            }
-        }
-        
-        this._msalInstance = new PublicClientApplication(msalConfig);
-     }
-    
-     login() {
-         const popupParam:PopupRequest = {
-             scopes: this._environmentService.scopes?.split(' ') ?? [],
-             prompt: "select_account"
-         }
-         return this._msalInstance.loginPopup(popupParam).then(async response => {
-            this._msalInstance.setActiveAccount(response.account);
+      }
 
-            this._activeHomeAccountId = response.account?.homeAccountId;
+      const isLoggedIn = !!user && user.homeAccountId == this._activeHomeAccountId;
 
-            await this.completeLogin();
-         });
-     }
+      if (this._activeHomeAccountId !== user?.localAccountId) {
+        this._loginChangedSubject.next(isLoggedIn);
+      }
 
-     isLoggedIn(): Promise<Boolean> {
-        return new Promise(async (resolve, reject) => {
-            let user = this._msalInstance.getActiveAccount();
+      return resolve(isLoggedIn);
+    });
+  }
 
-            if (user == null) {
-                const users = this._msalInstance.getAllAccounts();
+  completeLogin() {
+    return new Promise<AppUser>((resolve, reject) => {
+      const user = this._msalInstance.getActiveAccount();
 
-                if (users.length > 0) {
-                    user = users[0];
-                    this._activeHomeAccountId = user.homeAccountId
-                    this._msalInstance.setActiveAccount(user);
-                }
-            }
+      this._loginChangedSubject.next(!!user);
 
-            const isLoggedIn = !!user && user.homeAccountId == this._activeHomeAccountId;
+      if (user != null) {
+        this._activeHomeAccountId = user.homeAccountId;
+      }
 
-            if (this._activeHomeAccountId !== user?.localAccountId) {
-                this._loginChangedSubject.next(isLoggedIn);
-            }
+      return resolve(new AppUser());
+    });
+  }
 
-            return resolve(isLoggedIn);
-        });
-     }
+  logout() {
+    this._msalInstance.loginPopup().then(async response => {
+      await this.completeLogout();
+    });
+  }
 
-    completeLogin() {
-      return new Promise<AppUser>((resolve, reject) => {
-            const user = this._msalInstance.getActiveAccount();
+  completeLogout() {
+    return new Promise<AuthResponse>((resolve, reject) => {
+      this._activeHomeAccountId = undefined;
+      this._loginChangedSubject.next(false);
+      return resolve(new AuthResponse());
+    })
+  }
 
-            this._loginChangedSubject.next(!!user);
+  getAccessToken(): Promise<String | null> {
+    if (this._activeHomeAccountId == null) {
+      const user = this._msalInstance.getActiveAccount();
 
-            if (user != null) {
-                this._activeHomeAccountId = user.homeAccountId;
-            }
+      this._loginChangedSubject.next(!!user);
 
-            return resolve(new AppUser());
-        });
+      if (user != null) {
+        this._activeHomeAccountId = user.homeAccountId;
+      }
+      else {
+        return new Promise((resolve, reject) => resolve(null));
+      }
     }
 
-    logout() {
-        this._msalInstance.loginPopup().then(async response => {
-            await this.completeLogout();
-        });
-    }
+    const accountInfo = this._msalInstance.getAccountByHomeId(this._activeHomeAccountId) ?? undefined;
 
-    completeLogout() {
-        return new Promise<AuthResponse>((resolve, reject) => {
-            this._activeHomeAccountId = undefined;
-            this._loginChangedSubject.next(false);
-            return resolve(new AuthResponse());
-        })
-    }
+    const requestObj: SilentRequest = {
+      scopes: this._environmentService.scopes?.split(' ') ?? [],
+      authority: this._environmentService.stsAuthority,
+      account: accountInfo
+    };
 
-    getAccessToken() : Promise<String | null> {
-        if (this._activeHomeAccountId == null) {
-            const user = this._msalInstance.getActiveAccount();
-
-            this._loginChangedSubject.next(!!user);
-
-            if (user != null) {
-                this._activeHomeAccountId = user.homeAccountId;
-            }
-            else {
-                return new Promise((resolve, reject) => resolve(null));
-            }
-        }
-
-        const requestObj:SilentRequest = {
-          scopes: this._environmentService.scopes?.split(' ') ?? [],
-          authority: this._environmentService.stsAuthority
-        };
-
-        return this._msalInstance.acquireTokenSilent(requestObj).then(user => {
-          if (!!user && !!user.accessToken) {
-            return user.accessToken;
-          }
-          else {
-            return null;
-          }
-        })
-          .catch(reason => {
-            console.log(reason);
-            this._activeHomeAccountId = undefined;
-            this._msalInstance.setActiveAccount(null);
-            this._loginChangedSubject.next(false);
-            return null;
-          });
-    }
+    return this._msalInstance.acquireTokenSilent(requestObj).then(user => {
+      if (!!user && !!user.accessToken) {
+        return user.accessToken;
+      }
+      else {
+        return null;
+      }
+    })
+      .catch(reason => {
+        console.log(reason);
+        this._activeHomeAccountId = undefined;
+        this._msalInstance.setActiveAccount(null);
+        this._loginChangedSubject.next(false);
+        return null;
+      });
+  }
 }
