bbe75445670a229f3e5716fd23ea6f83f3f08723
Frédéric Jacques <gary.bob88@gmail.com>
2022-06-05 10:45:47 -0400

    fix version change in github action + add missing options class

---
+++ ErabliereAPI.Proxy/ErabliereAPIOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace ErabliereAPI.Proxy;
+
+public class ErabliereApiOptions
+{
+    public string BaseUrl { get; set; } = "";
+
+    public string TenantId { get; set; } = "";
+
+    public string ClientId { get; set; } = "";
+
+    public string ClientSecret { get; set; } = "";
+
+    public string Scope { get; set; } = "";
+}
