52f6d29b741b853d8e699db94ede69d0429cd626
Frédéric Jacques <gary.bob88@gmail.com>
2022-06-05 20:57:08 -0400

    update to cypress 10.0.2

---
+++ ErabliereIU/cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    specPattern: '**/*.spec.ts',
+    supportFile: "cypress/support/index.ts",
+    videosFolder: "cypress/videos",
+    screenshotsFolder: "cypress/screenshots",
+    fixturesFolder: "cypress/fixtures",
+    baseUrl: "https://localhost:4200",
+    projectId: "7cxkq4"
+  }
+})
