5253a0759045167972486f0cb603567bdc1011fa
Frédéric Jacques <gary.bob88@gmail.com>
2022-11-13 14:45:31 -0500

    edit extraireInfoHmi script to send to multiple apis

--- PythonScripts/extraireInfoHmi.py
+++ PythonScripts/extraireInfoHmi.py
@@ -65,15 +65,6 @@ r_niveaubassin = re.findall("HG \d+", text)
 
 print("niveau bassin", r_niveaubassin)
 
-# Envoie des données à l'api
-
-import json
-
-print("Envoie des données à l'api", sys.argv[2])
-
-url = sys.argv[2]
-idErabliere = sys.argv[3]
-
 vaccium = 0
 # Cas spéciaux de image_to_string
 if len(r_vaccium) > 0:
@@ -85,9 +76,20 @@ nb = 0
 if len(r_niveaubassin) > 0:
   nb = int(float(r_niveaubassin[0].replace("HG ", "")))
 
-proxy = ErabliereApiProxy(url, "AzureAD")
-reponse = proxy.envoyer_donnees(idErabliere, int(float(r_temperature[0].replace("°C", "")) * 10), vaccium, nb)
+# Envoie des données à l'api
+import json
 
-print("réponse", reponse.status_code)
+print("Envoie des données à l'api", sys.argv[2])
 
-print("terminé.", (dt.utcnow() - td(hours=5)).strftime("%m/%d/%Y, %H:%M:%S"))
+urls = sys.argv[2]
+idErabliere = sys.argv[3]
+
+def getDate():
+  return (dt.utcnow() - td(hours=5)).strftime("%m/%d/%Y, %H:%M:%S")
+
+for url in urls.split(","):
+  print('POST to', url, getDate())
+  proxy = ErabliereApiProxy(url, "AzureAD")
+  reponse = proxy.envoyer_donnees(idErabliere, int(float(r_temperature[0].replace("°C", "")) * 10), vaccium, nb)
+  print("réponse", reponse.status_code)
+  print("terminé.", getDate())
