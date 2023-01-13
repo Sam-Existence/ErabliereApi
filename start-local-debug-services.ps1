1330a989c74f1adf507e01efbdf683ec260afdb7
Frédéric Jacques <gary.bob88@gmail.com>
2023-01-13 08:21:59 -0500

    maj code repos avant demarrage des services, démarrage cypress

--- start-local-debug-services.ps1
+++ start-local-debug-services.ps1
@@ -1,3 +1,5 @@
+git status
+git pull
 
 Start-Process stripe -ArgumentList "listen", "--forward-to", "localhost:5000/Checkout/Webhook"
 
@@ -15,6 +17,8 @@ npm install
 Start-Process npm -ArgumentList "start"
 
 Start-Process npm -ArgumentList "run", "storybook"
+
+Start-Process npx -ArgumentList "cypress", "open"
 
 Set-Location ..
 
