0626a51b71d9083b98e8f75aab63c21383195445
Frédéric Jacques <gary.bob88@gmail.com>
2023-04-30 12:06:58 -0400

    add install.sh script

---
+++ PythonScripts/install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt install tesseract-ocr
+sudo apt-get install python-dev python-setuptools
+sudo apt-get install libjpeg-dev -y
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install libfreetype6-dev -y
+sudo apt-get install liblcms1-dev -y
+sudo apt-get install libopenjp2-7 -y
+sudo apt-get install libtiff5 -y
+sudo pip3 install pytesseract
+sudo pip3 install pillow
