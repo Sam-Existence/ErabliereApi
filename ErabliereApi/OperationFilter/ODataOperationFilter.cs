d30fa607cefbf0dc7b3c8a0933b40703b021402b
Frédéric Jacques <gary.bob88@gmail.com>
2023-09-02 16:33:52 -0400

    improve swagger - odata operation filter

--- ErabliereApi/OperationFilter/ODataOperationFilter.cs
+++ ErabliereApi/OperationFilter/ODataOperationFilter.cs
@@ -22,20 +22,20 @@ public class ODataOperationFilter : IOperationFilter
 
         if (hasODataAttribute)
         {
-            AddODataParameter("$select", format: "string", type: "string");
-            AddODataParameter("$filter");
-            AddODataParameter("$top", format: "int32", type: "integer");
-            AddODataParameter("$skip", format: "int32", type: "integer");
+            AddODataParameter("$select", format: "string", type: "string", examples: GetSelectExamples());
+            AddODataParameter("$filter", examples: GetFilterExamples());
+            AddODataParameter("$top", format: "int32", type: "integer", examples: GetTopExamples(), defaultValue: "10");
+            AddODataParameter("$skip", format: "int32", type: "integer", examples: GetSkipExamples());
 
             if (ExpandEnabled(enableQueryAttributes))
             {
-                AddODataParameter("$expand");
+                AddODataParameter("$expand", examples: GetExpandExamples());
             }
-            
-            AddODataParameter("$orderby", format: "string", type: "string"/*, GetOrderByExample()*/);
+
+            AddODataParameter("$orderby", format: "string", type: "string", GetOrderByExamples());
         }
 
-        void AddODataParameter(string name, string format = "expression", string type = "string", IOpenApiAny? example = default)
+        void AddODataParameter(string name, string format = "expression", string type = "string", Dictionary<string, OpenApiExample>? examples = default, string? defaultValue = null)
         {
             operation.Parameters.Add(new OpenApiParameter
             {
@@ -46,18 +46,101 @@ public class ODataOperationFilter : IOperationFilter
                     Format = format,
                     Type = type
                 },
-                Example = example
+                Examples = examples,
+                Example = defaultValue != null ? new OpenApiString(defaultValue) : null
             });
         }
     }
 
-    //private IOpenApiAny? GetOrderByExample()
-    //{
-    //    return new OpenApiExample
-    //    {
-    //        Value = new OpenApiString("")
-    //    };
-    //}
+    private Dictionary<string, OpenApiExample>? GetOrderByExamples()
+    {
+        var examples = new Dictionary<string, OpenApiExample>
+            {
+                { "empty", new OpenApiExample { Value = new OpenApiString("") } },
+                { "asc", new OpenApiExample { Value = new OpenApiString("propertyName") } },
+                { "desc", new OpenApiExample { Value = new OpenApiString("propertyName desc") } },
+                { "complexe sort", new OpenApiExample { Value = new OpenApiString("propertyOne asc, propertyTwo desc") } }
+            };
+
+        return examples;
+    }
+
+    private Dictionary<string, OpenApiExample>? GetExpandExamples()
+    {
+        var examples = new Dictionary<string, OpenApiExample>
+            {
+                { "empty", new OpenApiExample { Value = new OpenApiString("") } },
+                { "expand one property", new OpenApiExample { Value = new OpenApiString("propertyName") } },
+                { "expand multiple property", new OpenApiExample { Value = new OpenApiString("propertyOne,propertyTwo") } },
+                { "expand nested property", new OpenApiExample { Value = new OpenApiString("modelSourceProperty/childOneProperty/childTwoProperty") } }
+            };
+
+        return examples;
+    }
+
+    private Dictionary<string, OpenApiExample>? GetSkipExamples()
+    {
+        var examples = new Dictionary<string, OpenApiExample>
+            {
+                { "empty", new OpenApiExample { Value = new OpenApiString("") } },
+                { "skip 10", new OpenApiExample { Value = new OpenApiString("10") } }
+            };
+
+        return examples;
+    }
+
+    private Dictionary<string, OpenApiExample>? GetTopExamples()
+    {
+        var examples = new Dictionary<string, OpenApiExample>
+            {
+                { "empty", new OpenApiExample { Value = new OpenApiString("") } },
+                { "take 10", new OpenApiExample { Value = new OpenApiString("10") } }
+            };
+
+        return examples;
+    }
+
+    private Dictionary<string, OpenApiExample>? GetFilterExamples()
+    {
+        var examples = new Dictionary<string, OpenApiExample>
+            {
+                { "empty", new OpenApiExample { Value = new OpenApiString("") } },
+                { "equal", new OpenApiExample { Value = new OpenApiString("propertyName eq 'Some value'") } },
+                { "and", new OpenApiExample { Value = new OpenApiString("propertyOne eq 'Some value' and propertyTwo eq 'other Value'") } },
+                { "or", new OpenApiExample { Value = new OpenApiString("propertyOne eq 'Some value' or propertyTwo eq 'other Value'") } },
+                { "less than", new OpenApiExample { Value = new OpenApiString("propertyName lt 610") } },
+                { "greater than", new OpenApiExample { Value = new OpenApiString("propertyName gt 610") } },
+                { "less or equal than", new OpenApiExample { Value = new OpenApiString("propertyName le 610") } },
+                { "greater or equal than", new OpenApiExample { Value = new OpenApiString("propertyName ge 610") } },
+                { "not equal", new OpenApiExample { Value = new OpenApiString("propertyName ne 610") } },
+                { "endswith", new OpenApiExample { Value = new OpenApiString("endwith(propertyName, 'value') eq true") } },
+                { "startswith", new OpenApiExample { Value = new OpenApiString("startswith(propertyName, 'value') eq true") } },
+                { "contains", new OpenApiExample { Value = new OpenApiString("contains(propertyName, 'value') eq true") } },
+                { "indexof", new OpenApiExample { Value = new OpenApiString("indexof(propertyName, 'VALUE') eq 0") } },
+                { "replace", new OpenApiExample { Value = new OpenApiString("replace(propertyName, 'VALUE', 'VALUEREPLACED') eq 'OTHER VALUE'") } },
+                { "substring", new OpenApiExample { Value = new OpenApiString("substring(propertyName, 'OTHERVALUE') eq 'VALUE'") } },
+                { "tolower", new OpenApiExample { Value = new OpenApiString("tolower(propertyName) eq 'VALUE'") } },
+                { "toupper", new OpenApiExample { Value = new OpenApiString("toupper(propertyName) eq 'VALUE'") } },
+                { "trim", new OpenApiExample { Value = new OpenApiString("trim(propertyName) eq 'VALUE'") } },
+                { "concat", new OpenApiExample { Value = new OpenApiString("concat(concat(PropertyText1, ', '), PropertyText2) eq 'value1, value2'") } },
+                { "floor", new OpenApiExample { Value = new OpenApiString("floor(propertyDecimal) eq 1") } },
+                { "ceiling", new OpenApiExample { Value = new OpenApiString("ceiling(propertyDecimal) eq 1") } },
+            };
+
+        return examples;
+    }
+
+    private Dictionary<string, OpenApiExample>? GetSelectExamples()
+    {
+        var examples = new Dictionary<string, OpenApiExample>
+            {
+                { "empty", new OpenApiExample { Value = new OpenApiString("") } },
+                { "select one property", new OpenApiExample { Value = new OpenApiString("propertyName") } },
+                { "select multiple property", new OpenApiExample { Value = new OpenApiString("propertyOne,propertyTwo") } }
+            };
+
+        return examples;
+    }
 
     private bool ExpandEnabled(IEnumerable<EnableQueryAttribute> enableQueryAttributes)
     {
