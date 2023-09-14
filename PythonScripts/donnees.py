4d5b7584d634d4e8a8d6e18390f51c0ad1483422
Frédéric Jacques <gary.bob88@gmail.com>
2023-09-13 23:41:14 -0400

    fix id operation

--- PythonScripts/donnees.py
+++ PythonScripts/donnees.py
@@ -16,9 +16,9 @@ def temperature(t):
   hod = t.timetuple()[3]
   return int((12 * math.sin((doy-105)*math.pi/182.5)) + (5*math.sin(doy*4)) + (5 * math.sin((hod - 7) * 3.14159 / 12)) * 10)
 
-def getVaccium(id, t):
+def getVaccium(t):
   vaccium = 0
-  if t >= -1 - id:
+  if t >= -1:
     vaccium = 24.1
   if t > 3:
     vaccium += 1
@@ -34,9 +34,9 @@ print("Érablière :", idErabliere)
 t = temperature(datetime.datetime.utcnow())
 vaccium = 0
 print("La temperature est", t/10)
-if t >= -2 + id:
-  vaccium = getVaccium(id, t)
+if t >= -2:
+  vaccium = getVaccium(t)
 print("Le vaccium est", vaccium)
-r = proxy.envoyer_donnees(id, t, vaccium, niveaubassin = 0)
+r = proxy.envoyer_donnees(idErabliere, t, vaccium, niveaubassin = 0)
 print(r)
 print(r.text)
