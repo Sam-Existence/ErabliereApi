64b586d5d6203e186efcc1d4242c6a332819ce11
Frédéric Jacques <gary.bob88@gmail.com>
2023-12-30 16:25:19 -0500

    add custum theme to the storybook

---
+++ ErabliereIU/.storybook/erabliereapi-theme.js
@@ -0,0 +1,14 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'ErabliereIU Storybook',
+  brandUrl: 'https://erabliereapi.freddycoder.com',
+  brandImage: 'https://erabliereapi.freddycoder.com/assets/350-150-erabliereapi-logo.png',
+  brandTarget: '_self',
+  
+  appBg: '#0a261b',
+  appContentBg: '#0a261b',
+  appPreviewBg: '#0a261b',
+  barBg: '#0a261b',
+});
---
+++ ErabliereIU/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import erabliereapiTheme from './erabliereapi-theme';
+
+addons.setConfig({
+  theme: erabliereapiTheme,
+});
