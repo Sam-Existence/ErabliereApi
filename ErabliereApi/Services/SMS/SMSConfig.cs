0c29564a24ef3b1645f9372a344439b458a65fb0
Sam-Existence <SamFiBlackwell@gmail.com>
2024-03-14 12:20:15 -0400

    Ajout des classes pour envoyer des SMS

---
+++ ErabliereApi/Services/SMS/SMSConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace ErabliereApi;
+
+/// <summary>
+/// Modèle pour la configuration pour l'envoie de SMS. 
+/// Utilisé pour la fonctionnalité d'alertage
+/// </summary>
+public class SMSConfig
+{
+    /// <summary>
+    /// Le numéro de l'expéditeur
+    /// </summary>
+    public string? Numero { get; set; }
+
+    /// <summary>
+    /// Account SID d'application Twilio
+    /// </summary>
+    public string? AccountSid { get; set; }
+
+    /// <summary>
+    /// Auth token d'application Twilio
+    /// </summary>
+    public string? AuthToken { get; set; }
+
+    /// <summary>
+    /// Indique si la config est bien remplie
+    /// </summary>
+    public bool IsConfigured => Numero != null &&
+                                AccountSid != null &&
+                                AuthToken != null;
+}
