312c457dc919d1ccdc6a704a7c47353ca24b3958
ZacharyGagnon <zac08gag@gmail.com>
2024-04-02 10:41:35 -0400

    ajout de la table position graph

---
+++ ErabliereApi/Depot/Sql/Migrations/20240328105426_AddTablePositionGraph.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ErabliereApi.Depot.Sql.Migrations
+{
+    /// <summary>
+    /// Ajout de la table pour suavegarder les positions des graphiques
+    /// </summary>
+    public partial class AddTablePositionGraph : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PositionGraphs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdErabliere = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Nom = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    PositionX = table.Column<int>(type: "int", nullable: true),
+                    PositionY = table.Column<int>(type: "int", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PositionGraphs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PositionGraphs_Erabliere_IdErabliere",
+                        column: x => x.IdErabliere,
+                        principalTable: "Erabliere",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PositionGraphs_IdErabliere",
+                table: "PositionGraphs",
+                column: "IdErabliere");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PositionGraphs");
+        }
+    }
+}
