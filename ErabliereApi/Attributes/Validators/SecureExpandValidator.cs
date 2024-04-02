f02db8c9c37a49f816152991cd86283220a76715
Sam-Existence <SamFiBlackwell@gmail.com>
2024-04-02 11:46:57 -0400

    Ajout de SecureExpandValidator

---
+++ ErabliereApi/Attributes/Validators/SecureExpandValidator.cs
@@ -0,0 +1,32 @@
+﻿
+using ErabliereApi.Donnees;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Query.Validator;
+
+namespace ErabliereApi.Attributes.Validators
+{
+    public class SecureExpandValidator : SelectExpandQueryValidator
+    {
+        public override void Validate(SelectExpandQueryOption selectExpandQueryOption, ODataValidationSettings validationSettings)
+        {
+            string expand = selectExpandQueryOption.RawExpand;
+
+            if (expand.Contains(nameof(Erabliere.Alertes), StringComparison.OrdinalIgnoreCase))
+            {
+                throw new Microsoft.OData.ODataException($"La requête sur {nameof(Erabliere.Alertes)} n'est pas permise");
+            }
+
+            if (expand.Contains(nameof(Erabliere.Documentations), StringComparison.OrdinalIgnoreCase))
+            {
+                throw new Microsoft.OData.ODataException($"La requête sur {nameof(Erabliere.Documentations)} n'est pas permise");
+            }
+
+            if (expand.Contains(nameof(Erabliere.Notes), StringComparison.OrdinalIgnoreCase))
+            {
+                throw new Microsoft.OData.ODataException($"La requête sur {nameof(Erabliere.Notes)} n'est pas permise");
+            }
+
+            base.Validate(selectExpandQueryOption, validationSettings);
+        }
+    }
+}
