8d70da121deb6899644236df9db2d434dbe998be
Sam-Existence <SamFiBlackwell@gmail.com>
2024-04-02 11:47:15 -0400

    Ajout de SecureEnableQueryAttribute

---
+++ ErabliereApi/Attributes/SecureEnableQueryAttribute.cs
@@ -0,0 +1,14 @@
+﻿using ErabliereApi.Attributes.Validators;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace ErabliereApi.Attributes
+{
+    public class SecureEnableQueryAttribute : EnableQueryAttribute
+    {
+        public override void ValidateQuery(HttpRequest request, ODataQueryOptions queryOpts)
+        {
+            queryOpts.SelectExpand.Validator = new SecureExpandValidator();
+            base.ValidateQuery(request, queryOpts);
+        }
+    }
+}
