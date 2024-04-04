0be53e9c57d65f628c6aefb392f3c19497b2bf8a
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-04 11:10:44 -0400

    Add RappelsComponent HTML and TypeScript files

---
+++ ErabliereIU/src/rappel/rappels.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rappels',
+  standalone: true,
+  imports: [],
+  templateUrl: './rappels.component.html'
+})
+export class RappelsComponent {
+
+}
