20dfc35a81faab3db9922353641922557e260d4c
Frédéric Jacques <gary.bob88@gmail.com>
2024-04-05 09:38:52 -0400

    update to azire ai beta 15, add image generation endpoint

---
+++ ErabliereModel/Action/Post/PostImagesGenerationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErabliereApi.Donnees.Action.Post;
+
+
+public class PostImagesGenerationModel
+{
+    public int? ImageCount { get; set; }
+    public string Prompt { get; set; }
+    public string? Size { get; set; }
+}
