3c64e6bef884cece36435c8b82294a31798cb6f5
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-12 08:46:53 -0400

    Add getExcerpt method to RappelComponent

--- ErabliereIU/src/rappel/rappel.component.ts
+++ ErabliereIU/src/rappel/rappel.component.ts
@@ -14,4 +14,8 @@ export class RappelComponent {
   constructor() {
     this.note = new Note();
    }
+
+   getExcerpt(text: string, length: number = 100): string {
+    return text.length > length ? text.slice(0, length) + '...' : text;
+  }
 }
