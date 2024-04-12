1054471a322f8b2ca3a235995593ae1b881e3b5a
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-12 09:28:37 -0400

    Refactor RappelsComponent constructor and ngOnInit method

--- ErabliereIU/src/rappel/rappels.component.ts
+++ ErabliereIU/src/rappel/rappels.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { NgFor } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
 import { Note } from 'src/model/note';
 import { ErabliereApi } from 'src/core/erabliereapi.service';
 import { RappelComponent } from './rappel.component';
@@ -14,29 +13,16 @@ import { RappelComponent } from './rappel.component';
   ],
   templateUrl: './rappels.component.html'
 })
-export class RappelsComponent implements OnInit {
+export class RappelsComponent {
   @Input() idErabliereSelectionnee: any;
   todayReminders: Note[] = [];
 
-  constructor(private erabliereapiService: ErabliereApi, private _route: ActivatedRoute) { }
+  constructor(private erabliereapiService: ErabliereApi) { }
 
   async ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnChanges called');
     if (changes.idErabliereSelectionnee && changes.idErabliereSelectionnee.currentValue) {
       console.log('idErabliereSelectionnee changed', this.idErabliereSelectionnee);
-      try {
-        this.todayReminders = await this.getTodaysReminders(this.idErabliereSelectionnee);
-        console.log('todayReminders', this.todayReminders);
-      } catch (error) {
-        console.error('Error getting today\'s reminders', error);
-      }
-    }
-  }
-  
-  async ngOnInit() {
-    console.log('ngOnInit called');
-    if (this.idErabliereSelectionnee) {
-      console.log('idErabliereSelectionnee', this.idErabliereSelectionnee);
       try {
         this.todayReminders = await this.getTodaysReminders(this.idErabliereSelectionnee);
         console.log('todayReminders', this.todayReminders);
