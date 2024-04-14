77b0dae04431f19dbbad2f8bd48537806b469d5a
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-14 18:52:31 -0400

    Refactor RappelComponent to include ModalRappelComponent

--- ErabliereIU/src/rappel/rappel.component.ts
+++ ErabliereIU/src/rappel/rappel.component.ts
@@ -1,22 +1,39 @@
-import { Component, Input } from '@angular/core';
-import { th } from 'date-fns/locale';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { Modal } from 'bootstrap';
 import { Note } from 'src/model/note';
+import { ModalRappelComponent} from "./modal-rappel/modal-rappel.component";
 
 @Component({
   selector: 'app-rappel',
   standalone: true,
-  imports: [],
+    imports: [
+        ModalRappelComponent
+    ],
   styleUrls: ['./rappel.component.css'],
   templateUrl: './rappel.component.html'
 })
-export class RappelComponent {
+export class RappelComponent implements AfterViewInit {
   @Input() note: Note;
-
+  @Input() index!: number;
+  @ViewChild(ModalRappelComponent, { static: false }) modalRappelComponent!: ModalRappelComponent;
+  private modalInstance: any;
   constructor() {
     this.note = new Note();
-   }
+  }
 
-   getExcerpt(text: string | undefined, length: number = 100): string {
+    ngAfterViewInit(): void {
+        if (this.modalRappelComponent && this.modalRappelComponent.modalRappel) {
+            const modalElement = this.modalRappelComponent.modalRappel.nativeElement;
+            this.modalInstance = new Modal(modalElement);
+        } else {
+            console.error('ModalRappelComponent or modalRappel is not defined');
+        }
+    }
+
+    openModal(): void {
+        this.modalRappelComponent.openModal();
+    }
+  getExcerpt(text: string | undefined, length: number = 100): string {
     return text && text.length > length ? text.slice(0, length) + '...' : text || '';
   }
 }
