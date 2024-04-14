b904ef21a0eec25899491bd1d866558c9fef87e4
Frédéric Jacques <gary.bob88@gmail.com>
2024-04-14 08:12:15 -0400

    fix warning, fix unit test

--- ErabliereModel/Action/Post/PostImagesGenerationModel.cs
+++ ErabliereModel/Action/Post/PostImagesGenerationModel.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ErabliereApi.Donnees.Action.Post;
 
-namespace ErabliereApi.Donnees.Action.Post;
-
-
+/// <summary>
+/// Modèle pour la génération d'images
+/// </summary>
 public class PostImagesGenerationModel
 {
+    /// <summary>
+    /// Nombre d'images à générer
+    /// </summary>
     public int? ImageCount { get; set; }
-    public string Prompt { get; set; }
+
+    /// <summary>
+    /// La requête pour la génération d'image
+    /// </summary>
+    public string Prompt { get; set; } = "";
+
+    /// <summary>
+    /// Taille des images
+    /// </summary>
+    /// <example>100x100</example>
     public string? Size { get; set; }
 }
