c2b0ce3cd8ed88d3dccd363ebe6eecf42fd675fc
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-14 18:52:46 -0400

    Add modal functionality to ModalRappelComponent

--- ErabliereIU/src/rappel/modal-rappel/modal-rappel.component.ts
+++ ErabliereIU/src/rappel/modal-rappel/modal-rappel.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild, OnInit, AfterViewInit} from '@angular/core';
 import { Note } from 'src/model/note';
+import { Modal } from "bootstrap";
 
 @Component({
   selector: 'app-modal-rappel',
@@ -8,10 +9,26 @@ import { Note } from 'src/model/note';
   templateUrl: './modal-rappel.component.html',
   styleUrl: './modal-rappel.component.css'
 })
-export class ModalRappelComponent {
+export class ModalRappelComponent implements AfterViewInit {
     @Input() note: Note;
+    @ViewChild('modalRappel') modalRappel!: ElementRef;
+    private modalInstance: any;
+    @Input() index!: number;
 
     constructor() {
         this.note = new Note();
     }
+
+    ngAfterViewInit(): void {
+        const modalElement = this.modalRappel.nativeElement;
+        this.modalInstance = new Modal(modalElement);
+    }
+
+    openModal(): void {
+        if (this.modalInstance) {
+        this.modalInstance.show();
+    } else {
+        console.error('Modal instance is not defined');
+    }
+}
 }
