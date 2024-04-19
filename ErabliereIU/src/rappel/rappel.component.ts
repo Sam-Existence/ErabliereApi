ba2b75748cfc1c8e71d67b61130ed6090acefb0b
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-19 09:39:22 -0400

    Refactor RappelComponent to remove AfterViewInit interface and fix imports

--- ErabliereIU/src/rappel/rappel.component.ts
+++ ErabliereIU/src/rappel/rappel.component.ts
@@ -1,47 +1,31 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Modal } from 'bootstrap';
 import { Note } from 'src/model/note';
 import { ModalRappelComponent} from "./modal-rappel/modal-rappel.component";
 
 @Component({
-  selector: 'app-rappel',
-  standalone: true,
+    selector: 'app-rappel',
+    standalone: true,
     imports: [
         ModalRappelComponent
     ],
-  styleUrls: ['./rappel.component.css'],
-  templateUrl: './rappel.component.html'
+    styleUrls: ['./rappel.component.css'],
+    templateUrl: './rappel.component.html'
 })
-export class RappelComponent implements AfterViewInit {
-  @Input() note: Note;
-  @Input() index!: number;
-  @ViewChild(ModalRappelComponent, { static: false }) modalRappelComponent!: ModalRappelComponent;
-  private modalInstance: any;
-  constructor() {
-    this.note = new Note();
-  }
-
-    ngAfterViewInit(): void {
-        if (this.modalRappelComponent && this.modalRappelComponent.modalRappel) {
-            const modalElement = this.modalRappelComponent.modalRappel.nativeElement;
-            this.modalInstance = new Modal(modalElement);
-        } else {
-            console.error('ModalRappelComponent or modalRappel is not defined');
-        }
+export class RappelComponent {
+    @Input() note: Note;
+    @Input() index!: number;
+    @ViewChild(ModalRappelComponent, { static: false }) modalRappelComponent!: ModalRappelComponent;
+    private modalInstance: any;
+    constructor() {
+        this.note = new Note();
     }
 
-    openModal(): void {
-        if (!this.modalInstance && this.modalRappelComponent && this.modalRappelComponent.modalRappel) {
-            const modalElement = this.modalRappelComponent.modalRappel.nativeElement;
-            this.modalInstance = new Modal(modalElement);
-        }
-        if (this.modalInstance) {
-            this.modalRappelComponent.openModal();
-        } else {
-            console.error('ModalRappelComponent or modalRappel is not defined');
-        }
+    onModalInitialized(modalInstance: Modal): void {
+        this.modalInstance = modalInstance;
     }
-  getExcerpt(text: string | undefined, length: number = 100): string {
+
+    getExcerpt(text: string | undefined, length: number = 100): string {
     return text && text.length > length ? text.slice(0, length) + '...' : text || '';
-  }
+    }
 }
