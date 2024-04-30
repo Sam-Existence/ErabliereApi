9aff946446fb95302805da23dca75ed0b1310de5
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-30 10:25:09 -0400

    Add PostRappel class to ErabliereModel/Action/Post/PostRappel.cs

---
+++ ErabliereModel/Action/Post/PostRappel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ErabliereApi.Donnees.Action.Post
+{
+    public class PostRappel
+    {
+        /// <summary>
+        /// L'id de l'érablière
+        /// </summary>
+        public Guid? IdErabliere { get; set; }
+
+        /// <summary>
+        /// La date du rappel
+        /// </summary>
+        public DateTimeOffset? DateRappel { get; set; }
+
+        /// <summary>
+        /// La périodicité du rappel
+        /// </summary>
+        [MaxLength(20, ErrorMessage = "La périodicité du rappel ne peut pas dépasser 20 caractères.")]
+        public string? Periodicite { get; set; }
+    }
+}
