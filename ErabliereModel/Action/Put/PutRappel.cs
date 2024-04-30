7f0b2c064c338f8f320d386731d692d846c3142a
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-30 11:42:38 -0400

    Refactor PutRappel class to update comments and translations

--- ErabliereModel/Action/Put/PutRappel.cs
+++ ErabliereModel/Action/Put/PutRappel.cs
@@ -6,17 +6,17 @@ namespace ErabliereApi.Donnees.Action.Put
     public class PutRappel
     {
         /// <summary>
-        /// The ID of the erabliere
+        /// L'id de l'érablière
         /// </summary>
         public Guid IdErabliere { get; set; }
 
         /// <summary>
-        /// The date of the reminder
+        /// La date du rappel
         /// </summary>
         public DateTimeOffset? DateRappel { get; set; }
 
         /// <summary>
-        /// The periodicity of the reminder
+        /// La periodicité du rappel
         /// </summary>
         [MaxLength(20, ErrorMessage = "The periodicity of the reminder cannot exceed 20 characters.")]
         public string? Periodicite { get; set; }
