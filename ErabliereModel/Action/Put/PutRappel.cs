24857ca5956544e0efd645fa361037045ea8e63d
Anthony Provencher <tony.25.pro@gmail.com>
2024-04-30 11:05:50 -0400

    Add PutRappel class to ErabliereModel/Action/Put/PutRappel.cs

---
+++ ErabliereModel/Action/Put/PutRappel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ErabliereApi.Donnees.Action.Put
+{
+    public class PutRappel
+    {
+        /// <summary>
+        /// The ID of the erabliere
+        /// </summary>
+        public Guid IdErabliere { get; set; }
+
+        /// <summary>
+        /// The date of the reminder
+        /// </summary>
+        public DateTimeOffset? DateRappel { get; set; }
+
+        /// <summary>
+        /// The periodicity of the reminder
+        /// </summary>
+        [MaxLength(20, ErrorMessage = "The periodicity of the reminder cannot exceed 20 characters.")]
+        public string? Periodicite { get; set; }
+    }
+}
