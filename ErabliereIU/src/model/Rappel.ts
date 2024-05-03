11e91e0aec3dda790c0ceed455df775368270c59
Anthony Provencher <tony.25.pro@gmail.com>
2024-05-03 14:58:43 -0400

    Add Rappel model class

---
+++ ErabliereIU/src/model/Rappel.ts
@@ -0,0 +1,11 @@
+import {Note} from "./note";
+
+export class Rappel {
+    id: any;
+    idErabliere: any;
+    isActive?: boolean;
+    dateRappel?: string;
+    periodicite?: string;
+    noteId?: any;
+    note?: Note;
+}
