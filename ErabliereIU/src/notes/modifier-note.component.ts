d574b3de4514d798baa7f36f5ef8d6776c166f47
Félix Gason <isatis.snowfox@gmail.com>
2024-05-24 13:09:50 -0400

    Fix validation in ModifierNote

--- ErabliereIU/src/notes/modifier-note.component.ts
+++ ErabliereIU/src/notes/modifier-note.component.ts
@@ -1,10 +1,10 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { ErabliereApi } from "src/core/erabliereapi.service";
-import { UntypedFormGroup, UntypedFormBuilder, FormControl, Validators, ReactiveFormsModule } from "@angular/forms";
-import { Note } from "src/model/note";
-import { InputErrorComponent } from "../formsComponents/input-error.component";
-import { NgIf } from "@angular/common";
-import { Subject } from "rxjs";
+import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {ErabliereApi} from "src/core/erabliereapi.service";
+import {UntypedFormGroup, UntypedFormBuilder, FormControl, Validators, ReactiveFormsModule} from "@angular/forms";
+import {Note} from "src/model/note";
+import {InputErrorComponent} from "../formsComponents/input-error.component";
+import {NgIf} from "@angular/common";
+import {Subject} from "rxjs";
 
 @Component({
     selector: 'modifier-note',
@@ -21,55 +21,56 @@ export class ModifierNoteComponent implements OnInit {
         this.noteSubject?.subscribe(note => {
             this.initializeForm();
             if (note) {
-                this.note = { ... note };
+                this.note = {...note};
                 if (this.note) {
                     this.noteForm.controls['title'].setValue(this.note.title);
                     this.noteForm.controls['text'].setValue(this.note.text);
                     this.noteForm.controls['noteDate'].setValue(this.note.noteDate);
-                    this.noteForm.controls['reminderDate'].setValue(this.note.reminderDate ? new Date(this.note.reminderDate).toISOString().split('T')[0] : '');                }
+                    this.noteForm.controls['reminderDate'].setValue(this.note.reminderDate ? new Date(this.note.reminderDate).toISOString().split('T')[0] : '');
+                }
             }
         });
     }
 
     initializeForm() {
-      this.noteForm = this.fb.group({
-        title: new FormControl(
-          '',
-          {
-            validators: [Validators.required, Validators.maxLength(200)],
-            updateOn: 'blur'
-          }),
-        text: new FormControl(
-          '',
-          {
-            validators: [Validators.maxLength(2000)],
-            updateOn: 'blur'
-          }),
-        file: new FormControl(
-          '',
-          {
-            updateOn: 'blur'
-          }
-        ),
-        fileBase64: new FormControl(
-          '',
-          {
-            updateOn: 'blur'
-          }
-        ),
-        noteDate: new FormControl(
-          '',
-          {
-            updateOn: 'blur'
-          }
-        ),
-        reminderDate: new FormControl(
-          '',
-          {
-            updateOn: 'blur'
-          }
-        ),
-      });
+        this.noteForm = this.fb.group({
+            title: new FormControl(
+                '',
+                {
+                    validators: [Validators.required, Validators.maxLength(200)],
+                    updateOn: 'blur'
+                }),
+            text: new FormControl(
+                '',
+                {
+                    validators: [Validators.maxLength(2000)],
+                    updateOn: 'blur'
+                }),
+            file: new FormControl(
+                '',
+                {
+                    updateOn: 'blur'
+                }
+            ),
+            fileBase64: new FormControl(
+                '',
+                {
+                    updateOn: 'blur'
+                }
+            ),
+            noteDate: new FormControl(
+                '',
+                {
+                    updateOn: 'blur'
+                }
+            ),
+            reminderDate: new FormControl(
+                '',
+                {
+                    updateOn: 'blur'
+                }
+            ),
+        });
     }
 
     error: string | null = null;
@@ -90,12 +91,12 @@ export class ModifierNoteComponent implements OnInit {
 
     generalError?: string | null;
 
-    today = new Intl.DateTimeFormat('fr-ca', { year: 'numeric', month: '2-digit', day: '2-digit' }).format(new Date());
+    today = new Intl.DateTimeFormat('fr-ca', {year: 'numeric', month: '2-digit', day: '2-digit'}).format(new Date());
 
     validateForm() {
-      const form = document.getElementById('modifier-note');
-      this.noteForm.updateValueAndValidity();
-      form?.classList.add('was-validated');
+        const form = document.getElementById('modifier-note');
+        this.noteForm.updateValueAndValidity();
+        form?.classList.add('was-validated');
     }
 
     onButtonAnnuleClick() {
@@ -104,63 +105,55 @@ export class ModifierNoteComponent implements OnInit {
 
     onButtonModifierClick() {
         if (this.note) {
-          if(this.noteForm.valid) {
-            this.note.title = this.noteForm.controls['title'].value;
-            this.note.text = this.noteForm.controls['text'].value;
-            if (this.noteForm.controls['noteDate'].value != "") {
-              this.note.noteDate = this.noteForm.controls['noteDate'].value;
-            }
-            else {
-              this.note.noteDate = null;
-            }
-            if (this.noteForm.controls['reminderDate'].value !== "") {
-              this.note.reminderDate = this.noteForm.controls['reminderDate'].value;
-            }
-            else {
-              this.note.reminderDate = null;
-            }
-            this._api.putNote(this.idErabliereSelectionee, this.note)
-              .then(r => {
-                this.errorObj = null;
-                this.fileToLargeErrorMessage = null;
-                this.generalError = null;
-                this.noteForm.reset();
-                this.needToUpdate.emit();
-                this.noteSubject?.next(null);
-                this.note = null;
-              })
-              .catch(e => {
-                if (e.status == 400) {
-                  this.errorObj = e
-                  this.fileToLargeErrorMessage = null;
-                  this.generalError = null;
-                }
-                else if (e.status == 404) {
-                  this.errorObj = null;
-                  this.fileToLargeErrorMessage = null;
-                  this.generalError = "L'érablière n'existe pas."
-                }
-                else if (e.status == 405) {
-                  this.errorObj = null;
-                  this.fileToLargeErrorMessage = null;
-                  this.generalError = "L'API ne permet pas de modifier une note."
-                }
-                else if (e.status == 413) {
-                  this.errorObj = null;
-                  this.fileToLargeErrorMessage = "Le fichier est trop gros."
-                  this.generalError = null;
-                }
-                else {
-                  this.errorObj = null;
-                  this.fileToLargeErrorMessage = null;
-                  this.generalError = "Une erreur est survenue."
-                }
-              });
-          } else {
             this.validateForm();
-          }
-        }
-        else {
+            if (this.noteForm.valid) {
+                this.note.title = this.noteForm.controls['title'].value;
+                this.note.text = this.noteForm.controls['text'].value;
+                if (this.noteForm.controls['noteDate'].value != "") {
+                    this.note.noteDate = this.noteForm.controls['noteDate'].value;
+                } else {
+                    this.note.noteDate = null;
+                }
+                if (this.noteForm.controls['reminderDate'].value !== "") {
+                    this.note.reminderDate = this.noteForm.controls['reminderDate'].value;
+                } else {
+                    this.note.reminderDate = null;
+                }
+                this._api.putNote(this.idErabliereSelectionee, this.note)
+                    .then(r => {
+                        this.errorObj = null;
+                        this.fileToLargeErrorMessage = null;
+                        this.generalError = null;
+                        this.noteForm.reset();
+                        this.needToUpdate.emit();
+                        this.noteSubject?.next(null);
+                        this.note = null;
+                    })
+                    .catch(e => {
+                        if (e.status == 400) {
+                            this.errorObj = e
+                            this.fileToLargeErrorMessage = null;
+                            this.generalError = null;
+                        } else if (e.status == 404) {
+                            this.errorObj = null;
+                            this.fileToLargeErrorMessage = null;
+                            this.generalError = "L'érablière n'existe pas."
+                        } else if (e.status == 405) {
+                            this.errorObj = null;
+                            this.fileToLargeErrorMessage = null;
+                            this.generalError = "L'API ne permet pas de modifier une note."
+                        } else if (e.status == 413) {
+                            this.errorObj = null;
+                            this.fileToLargeErrorMessage = "Le fichier est trop gros."
+                            this.generalError = null;
+                        } else {
+                            this.errorObj = null;
+                            this.fileToLargeErrorMessage = null;
+                            this.generalError = "Une erreur est survenue."
+                        }
+                    });
+            }
+        } else {
             console.log("this.note is null");
         }
     }
