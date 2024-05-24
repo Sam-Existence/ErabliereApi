700154a0737b2e9f0c3511282582a33c113a972d
Félix Gason <isatis.snowfox@gmail.com>
2024-05-24 13:07:12 -0400

    Add Ordre column to CapteurImage

---
+++ ErabliereApi/Depot/Sql/Migrations/20240524163657_AddOrdreColumnToCapteurImage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Depot.Sql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrdreColumnToCapteurImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Ordre",
+                table: "CapteurImage",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ordre",
+                table: "CapteurImage");
+        }
+    }
+}
