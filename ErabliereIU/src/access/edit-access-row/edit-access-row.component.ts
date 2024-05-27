09b70993b178b8d3c958b3e16c7a042d6343e289
Sam-Existence <SamFiBlackwell@gmail.com>
2024-05-26 20:40:52 -0400

    Ajout du composant edit-access-row

---
+++ ErabliereIU/src/access/edit-access-row/edit-access-row.component.ts
@@ -0,0 +1,54 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CustomerAccess} from "../../model/customerAccess";
+import {FormBuilder, FormControl, ReactiveFormsModule, UntypedFormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'tr[edit-access-row]',
+  standalone: true,
+    imports: [
+        ReactiveFormsModule
+    ],
+  templateUrl: './edit-access-row.component.html'
+})
+export class EditAccessRowComponent implements OnInit {
+    @Input() acces?: CustomerAccess;
+
+    @Output() accesAAnnuler = new EventEmitter<CustomerAccess>();
+    @Output() accesAEnregister = new EventEmitter<CustomerAccess>();
+
+    formGroup: UntypedFormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+        this.formGroup = this.formBuilder.group({
+            lecture: new FormControl<boolean>(false),
+            creation: new FormControl<boolean>(false),
+            modification: new FormControl<boolean>(false),
+            suppression: new FormControl<boolean>(false)
+        });
+    }
+
+    ngOnInit() {
+        if (this.acces?.access) {
+            this.formGroup.controls.lecture.setValue(!!(this.acces.access & 1));
+            this.formGroup.controls.creation.setValue(!!(this.acces.access & 2));
+            this.formGroup.controls.modification.setValue(!!(this.acces.access & 4));
+            this.formGroup.controls.suppression.setValue(!!(this.acces.access & 8));
+        }
+    }
+
+    signalerEnregistrement() {
+        if (this.acces && this.formGroup?.valid) {
+            this.acces.access =
+                (this.formGroup?.controls.lecture.value ? 1 : 0)
+            +   (this.formGroup.controls.creation.value ? 2 : 0)
+            +   (this.formGroup.controls.modification.value ? 4 : 0)
+            +   (this.formGroup.controls.suppression.value ? 8 : 0);
+
+            this.accesAEnregister.emit(this.acces);
+        }
+    }
+
+    signalerAnnulation() {
+        this.accesAAnnuler.emit(this.acces);
+    }
+}
