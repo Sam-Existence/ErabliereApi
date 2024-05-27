b7e4685982c2dbd8c138da54ea9b46b3aaf9c823
Sam-Existence <samfiblackwell@gmail.com>
2024-05-27 17:48:35 -0400

    Ajout de la gestion administrative de droits d'accès d'érablières

--- ErabliereIU/src/admin/admin-erablieres/erabliere-list/erabliere-list.component.ts
+++ ErabliereIU/src/admin/admin-erablieres/erabliere-list/erabliere-list.component.ts
@@ -1,10 +1,14 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Erabliere} from "../../../model/erabliere";
+import {AdminErabliereAccessListComponent} from "../../../access/admin-erabliere-access-list.component";
+import {CustomerAccess} from "../../../model/customerAccess";
 
 @Component({
     selector: 'erabliere-list',
     standalone: true,
-    imports: [],
+    imports: [
+        AdminErabliereAccessListComponent
+    ],
     templateUrl: './erabliere-list.component.html',
     styleUrl: './erabliere-list.component.css',
 
@@ -13,6 +17,16 @@ export class ErabliereListComponent {
     @Input() erablieres: Erabliere[] = [];
     @Output() erabliereASupprimer: EventEmitter<Erabliere> = new EventEmitter();
     @Output() erabliereAModifier: EventEmitter<Erabliere> = new EventEmitter();
+
+    showAccess: { [id: string]: boolean } = {}
+
+    toggleAccess(id: string): void {
+        this.showAccess[id] = !this.showAccess[id];
+    }
+
+    MAJAcces(acces: CustomerAccess[], erabliere: Erabliere) {
+        erabliere.customerErablieres = acces;
+    }
 
     signalerSuppression(erabliere: Erabliere) {
         this.erabliereASupprimer.emit(erabliere);
