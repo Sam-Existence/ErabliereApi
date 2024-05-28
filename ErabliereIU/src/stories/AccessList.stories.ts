f6d686ad5b5af3ed650d1e751e1085782a32bf84
Sam-Existence <SamFiBlackwell@gmail.com>
2024-05-28 09:33:34 -0400

    Correction des bugs

--- ErabliereIU/src/stories/AccessList.stories.ts
+++ ErabliereIU/src/stories/AccessList.stories.ts
@@ -18,5 +18,44 @@ export default meta;
 type Story = StoryObj<ErabliereAccessListComponent>;
 
 export const Primary: Story = {
-
+    args: {
+        customersAccess: [
+            {
+                idErabliere: "366912bb-cdbf-44a3-93ec-642e02997685",
+                idCustomer: "0a215c38-233f-494e-81d4-997c5acb8abc",
+                customer: {
+                    name: "James Smith",
+                    email: "james@smith.com"
+                },
+                access: 15
+            },
+            {
+                idErabliere: "366912bb-cdbf-44a3-93ec-622e02997685",
+                idCustomer: "0a215c38-233f-494e-81d4-927c5acb8abc",
+                customer: {
+                    name: "John Smith",
+                    email: "john@smith.com"
+                },
+                access: 8
+            },
+            {
+                idErabliere: "366912bb-cdbf-44a3-93ec-642e03997685",
+                idCustomer: "0a215c38-233f-494e-81d4-997c5a3b8abc",
+                customer: {
+                    name: "Jane Smith",
+                    email: "jane@smith.com"
+                },
+                access: 7
+            },
+            {
+                idErabliere: "366912bb-cdbf-44a3-93ac-642e02997685",
+                idCustomer: "0a215c38-233f-494e-81da-997c5acb8abc",
+                customer: {
+                    name: "Joel Smith",
+                    email: "joel@smith.com"
+                },
+                access: 3
+            }
+        ]
+    }
 };
