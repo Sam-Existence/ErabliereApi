4969e9019d917eeef68a50441fceb465f15512f4
Sam-Existence <samfiblackwell@gmail.com>
2024-05-28 00:06:51 -0400

    Ajout du composant view-customer-access-row

---
+++ ErabliereIU/src/access/customer-access-list/view-customer-access-row/view-customer-access-row.component.ts
@@ -0,0 +1,36 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {CustomerAccess} from "../../../model/customerAccess";
+
+@Component({
+  selector: 'tr[view-customer-access-row]',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-customer-access-row.component.html'
+})
+export class ViewCustomerAccessRowComponent implements OnInit {
+    @Input() acces?: CustomerAccess;
+    lecture: boolean = false;
+    creation: boolean = false;
+    modification: boolean = false;
+    suppression: boolean = false;
+
+    @Output() accesASupprimer = new EventEmitter<CustomerAccess>();
+    @Output() accesAModifier = new EventEmitter<CustomerAccess>();
+
+    ngOnInit() {
+        if (this.acces?.access) {
+            this.lecture = !!(this.acces.access & 1);
+            this.creation = !!(this.acces.access & 2);
+            this.modification = !!(this.acces.access & 4);
+            this.suppression = !!(this.acces.access & 8);
+        }
+    }
+
+    signalerSupression() {
+        this.accesASupprimer.emit(this.acces);
+    }
+
+    signalerModification() {
+        this.accesAModifier.emit(this.acces);
+    }
+}
