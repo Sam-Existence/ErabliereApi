34d525fa0569e8cffd652e47b3b25f5db83ea8c6
Félix Gason <isatis.snowfox@gmail.com>
2024-05-28 11:03:37 -0400

    Add capteur image list to capteur page

--- ErabliereIU/src/capteurs/gestion-capteurs.component.ts
+++ ErabliereIU/src/capteurs/gestion-capteurs.component.ts
@@ -5,16 +5,19 @@ import { CapteurListComponent } from "./capteur-list.component";
 import { AjouterCapteurComponent } from "./ajouter-capteur.component";
 import {AjouterCapteurImageComponent} from "./ajouter-capteur-image.component";
 import { NgIf } from "@angular/common";
+import {CapteurImage} from "../model/capteurImage";
+import {CapteurImageListComponent} from "./capteur-image-list.component";
 
 @Component({
     selector: 'gestion-capteurs',
     templateUrl: 'gestion-capteurs.component.html',
     standalone: true,
-    imports: [NgIf, AjouterCapteurComponent, CapteurListComponent, AjouterCapteurImageComponent]
+    imports: [NgIf, AjouterCapteurComponent, CapteurListComponent, AjouterCapteurImageComponent, CapteurImageListComponent]
 })
 export class GestionCapteursComponent implements OnInit {
     @Input() idErabliereSelectionee?: any;
     capteurs: Capteur[] = [];
+    capteursImage: CapteurImage[] = [];
     afficherSectionAjouterCapteur: boolean = false;
     afficherSectionAjouterCapteurImage: boolean = false;
 
@@ -23,6 +26,7 @@ export class GestionCapteursComponent implements OnInit {
     }
     async ngOnInit()  {
         await this.getCapteurs();
+        await this.getCapteursImage();
     }
 
     showAjouterCapteur() {
@@ -38,6 +42,14 @@ export class GestionCapteursComponent implements OnInit {
             this._api.getCapteurs(this.idErabliereSelectionee).then(capteurs => {
                 this.capteurs = capteurs;
             });
+        }
+    }
+
+    async getCapteursImage() {
+        if(this.idErabliereSelectionee) {
+            this._api.getCapteursImage(this.idErabliereSelectionee).then(capteurs => {
+                this.capteursImage = capteurs;
+            })
         }
     }
 }
